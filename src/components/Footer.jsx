8be89f7825a199a7d318f53f577c2c339eda2f05
Prabhjyot Singh <prabhjyotg@gmail.com>
2022-04-09 00:59:10 -0400

    Footer Component Text-alignment done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,38 @@ import React, { useState } from 'react';
 import "../views/Footer.css";
 
 function Footer() {
-    
+    const col1= [
+           "Alkali Metals",
+           "Metalloids",
+           "Actinides",
+        ];
+    const col2= [
+            "Alkaline earth metals",
+            "Reactive nonmetals",
+            "Unknown properties",
+        ];
+    const col3= [
+            "Transition Metals",
+            "Noble gases",
+        ];
+    const col4= [
+            "Post-transition metals",
+            "Lanthanides",
+        ];
+
     return <div className='footer'>
-        <h1>Footer</h1>
+        <div className='footerCol'>
+            {col1.map(col =><h7 className="footerRow__elements">{col}</h7>)}
+        </div>
+        <div className='footerCol'>
+            {col2.map(col =><h7 className="footerRow__elements">{col}</h7>)}
+        </div>
+        <div className='footerCol'>
+            {col3.map(col =><h7 className="footerRow__elements">{col}</h7>)}
+        </div>
+        <div className='footerCol'>
+            {col4.map(col =><h7 className="footerRow__elements">{col}</h7>)}
+        </div>
     </div>;
 }
 
