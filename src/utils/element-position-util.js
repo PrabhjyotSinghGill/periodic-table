f05655cc37d2ae3bb153e88905126f188674af17
Prabhjyot Singh <prabhjyotg@gmail.com>
2022-04-11 21:38:42 -0400

    Boron Period fixed to 2

--- src/utils/element-position-util.js
+++ src/utils/element-position-util.js
@@ -2,6 +2,9 @@ let elementPositionDict = {};
 
 export const initElementPositionDict = (elements) => {
     elements.forEach(element => {
+        if(element.symbol == 'B'){
+            element.period = 2;
+          }
         let key = `${element.period-1}-${element.group-1}`
         elementPositionDict[key] = element;
     });
